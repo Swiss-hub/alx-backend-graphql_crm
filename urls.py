690c7527cb44e3e188a889fbbf739a5e8ccdeaf5
Ilechukwu Francis C <ilechukwufran6x@gmail.com>
2025-07-06 23:42:03 +0100

    Set Up GraphQL Endpoint

--- urls.py
+++ urls.py
@@ -3,5 +3,7 @@ from graphene_django.views import GraphQLView
 from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
-    path("graphql", csrf_exempt(GraphQLView.as_view(graphiql=True)))),
+  path("graphql", 
+    csrf_exempt(GraphQLView.as_view(graphiql=True))
+  ),
 ]
