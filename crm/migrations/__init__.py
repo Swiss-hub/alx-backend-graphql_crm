5657a53f9963458be7e87e149d34cdcdd0b1bb23
Ilechukwu Francis C <ilechukwufran6x@gmail.com>
2025-07-07 01:38:25 +0100

    Task 1: Build and Seed a CRM Database with GraphQL Integration

--- crm/migrations/__init__.py
+++ crm/migrations/__init__.py
@@ -1,12 +1,1 @@
 # This file makes the migrations directory a Python package 
-
-import graphene
-from crm.schema import Query as CRMQuery, Mutation as CRMMutation
-
-class Query(CRMQuery, graphene.ObjectType):
-    pass
-
-class Mutation(CRMMutation, graphene.ObjectType):
-    pass
-
-schema = graphene.Schema(query=Query, mutation=Mutation) 
